60d3ffd48455949c45482d197b52911db33eac28
E4vnS09 <afonsorio1500@gmail.com>
2026-05-01 18:38:04 +0100

    MPI addition

---
+++ real_lab2/run_mpi.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+
+dir="times/MPI"
+
+if [[ ! -d $dir ]]
+then
+    mkdir $dir
+fi
+
+for (( m_size=$1; m_size<=$2; m_size+=$3 ))
+do
+	echo "Doing game of Life of matrix size ${m_size}x${m_size}"
+
+
+    if [[ ! -d $dir ]]
+    then
+        mkdir $dir
+    fi
+
+    tmp_file=$dir/tmp_file.tmp
+	time_file=$dir/GoL_Serial_${m_size}x${m_size}.time
+
+    echo '' > $tmp_file
+    echo '' > $time_file
+
+    for (( gen=0; gen<=$4; gen+=1 ))
+    do
+        (time mpiexec -v --host fct-deei-linux:1 --host fct-deei-aval:1 -np 2 -wd /home/a76943/SPD_26/GoL_MPI ./GoL_MPI/Life --no-display -r $m_size -c $m_size -g 128)  |& grep "real" | sed 's/,/./' >> $tmp_file
+    done
+
+    ./bin/make_time_file $tmp_file $time_file
+
+    rm $tmp_file
+
+done
